7c14b73c8a06bc96c8aa5a77f94f5f4fa6ffa338
Raphael Marques <raphaeljcmarques@gmail.com>
2022-07-17 13:40:42 -0300

    feat: adding images and default style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World!</h1>
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
