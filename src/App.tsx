4af66c7b4c5d92a2f8af5adeb00d88d9b61c4d72
Raphael Marques <raphaeljcmarques@gmail.com>
2022-07-17 11:38:54 -0300

    feat: setting up theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-export function App() {
-  return <h1>Hello World</h1>;
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World!</h1>
+    </ThemeProvider>
+  );
 }
+
+export default App;
