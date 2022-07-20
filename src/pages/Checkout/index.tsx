894969828bfccfa5e135bc432603ec42146758d9
Raphael Marques <raphaeljcmarques@gmail.com>
2022-07-19 21:43:17 -0300

    wip-feat: Product Counter Component and Checkout Page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,72 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react';
+import { Form, FormContainer, Input } from './styles';
+
 export function Checkout() {
-  return <h2>Checkout</h2>;
+  return (
+    <Form className="container">
+      <section>
+        <h2>Complete seu pedido</h2>
+        <div>
+          <FormContainer>
+            <div>
+              <MapPinLine color="#C47F17" size={22} />
+              <div>
+                <h3>Endereço de entrega</h3>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </div>
+
+            <div>
+              <Input type="number" name="cep" required placeholder="CEP" />
+              <Input type="text" name="street" required placeholder="Rua" />
+              <Input
+                type="number"
+                name="number"
+                required
+                placeholder="Número"
+              />
+              <Input type="text" name="complement" placeholder="Complemento" />
+              <Input type="text" name="city" required placeholder="Bairro" />
+              <Input type="text" name="state" required placeholder="Cidade" />
+              <Input type="text" name="acronym" required placeholder="UF" />
+            </div>
+          </FormContainer>
+          <FormContainer>
+            <div>
+              <CurrencyDollar color="#8047F8" size={22} />
+              <div>
+                <h3>Pagamento</h3>
+                <p>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </div>
+            <div>
+              <button type="button">
+                <CreditCard /> Cartão de Crédito
+              </button>
+              <button type="button">
+                <Bank /> Cartão de Débito
+              </button>
+              <button type="button">
+                <Money />
+                Dinheiro
+              </button>
+            </div>
+          </FormContainer>
+        </div>
+      </section>
+      <section>
+        <h2>Cafés selecionados</h2>
+        <div />
+      </section>
+    </Form>
+  );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,1 +1,86 @@
-export {};
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 2rem;
+
+  h2 {
+    color: ${props => props.theme['base-subtitle']};
+    font-size: 1.125rem;
+    line-height: 130%;
+    font-weight: 700;
+    font-family: 'Baloo 2', sans-serif;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const FormContainer = styled.div`
+  background-color: ${props => props.theme['base-card']};
+  border-radius: 6px;
+  padding: 2.5rem;
+
+  &:first-child {
+    margin-bottom: 0.75rem;
+  }
+
+  > div:first-child {
+    display: flex;
+    gap: 0.5rem;
+    margin-bottom: 1rem;
+
+    h3 {
+      line-height: 130%;
+      color: ${props => props.theme['base-subtitle']};
+      font-weight: 400;
+      font-size: 1rem;
+    }
+
+    p {
+      line-height: 130%;
+      font-size: 0.875rem;
+      color: ${props => props.theme['base-text']};
+    }
+  }
+
+  > div:last-child {
+    display: flex;
+    column-gap: 1rem;
+    row-gap: 0.75rem;
+  }
+`;
+
+export const Input = styled.input`
+  background-color: ${props => props.theme['base-input']};
+  border: 1px solid ${props => props.theme['base-button']};
+  padding: 0.75rem;
+  border-radius: 4px;
+  color: ${props => props.theme['base-text']};
+  font-size: 0.875rem;
+
+  // HIDE ARROWS FROM INPUT NUMBER
+  /* Chrome, Safari, Edge, Opera */
+  &[type='number']::-webkit-outer-spin-button,
+  &[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  &[type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  ::placeholder {
+    color: ${props => props.theme['base-label']};
+  }
+
+  &:nth-child(1),
+  &:nth-child(3),
+  &:nth-child(5) {
+    max-width: 12.5rem;
+  }
+  &:last-child {
+    max-width: 3.75rem;
+  }
+`;
