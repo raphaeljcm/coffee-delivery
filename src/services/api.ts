d80139054f653371e957889b659fa8d9d1ab2408
Raphael Marques <raphaeljcmarques@gmail.com>
2022-07-24 17:41:13 -0300

    refactor: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://coffee-delivery-backend.herokuapp.com/',
-  baseURL: 'http://localhost:3333/',
+  baseURL: 'https://coffee-delivery-backend.herokuapp.com/',
 });
 
 export default api;
