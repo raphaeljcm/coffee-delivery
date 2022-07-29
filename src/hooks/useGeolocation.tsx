2964e6fb126ead832891a9cc8f2f6496c80b2b3f
Raphael Marques <raphaeljcmarques@gmail.com>
2022-07-28 21:41:35 -0300

    test: useGeolocation

--- src/hooks/useGeolocation.tsx
+++ src/hooks/useGeolocation.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 
+interface locationData {
+  position: string;
+  error?: string;
+}
+
 export const useGeolocation = () => {
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState({} as locationData);
 
   useEffect(() => {
     if ('geolocation' in navigator) {
@@ -11,10 +16,10 @@ export const useGeolocation = () => {
           const location = await fetch(
             `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=35f709f4ef1e401c8ad997c809b4a6a3`
           ).then(res => res.json());
-          setLocation(location.results[0].formatted);
+          setLocation({ position: location.results[0].formatted });
         },
         error => {
-          setLocation(error.message);
+          setLocation({ position: '', error: error.message });
         }
       );
     } else {
