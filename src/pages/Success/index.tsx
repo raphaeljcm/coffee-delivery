d234ddadad1db322c21d0bdbbf934ddaa34c3f64
Raphael Marques <raphaeljcmarques@gmail.com>
2022-07-30 22:32:12 -0300

    style: Success Page styles done

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,51 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react';
+import * as S from './styles';
+import successImg from '../../assets/success-illustration.png';
+
 export function Success() {
-  return <h2>Success</h2>;
+  return (
+    <S.SuccessContainer className="container">
+      <h2>Uhu! Pedido confirmado</h2>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+
+      <div>
+        <div>
+          <div>
+            <S.SuccessDetails color="purple">
+              <MapPin weight="fill" size={16} color="#FAFAFA" />
+            </S.SuccessDetails>
+            <div>
+              <p>
+                Entrega em{' '}
+                <span className="bold">Rua João Daniel Martinelli, 102</span>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </div>
+          <div>
+            <S.SuccessDetails color="yellow">
+              <Timer weight="fill" size={16} color="#FAFAFA" />
+            </S.SuccessDetails>
+            <div>
+              <p>Previsão de entrega</p>
+              <p className="bold">20 min - 30 min</p>
+            </div>
+          </div>
+          <div>
+            <S.SuccessDetails color="yellow-dark">
+              <CurrencyDollar weight="fill" size={16} color="#FAFAFA" />
+            </S.SuccessDetails>
+            <div>
+              <p>Pagamento na entrega</p>
+              <p className="bold">Cartão de Crédito</p>
+            </div>
+          </div>
+        </div>
+        <img
+          src={successImg}
+          alt="Uma pessoa em cima de uma moto roxa com o pedido na garupa"
+        />
+      </div>
+    </S.SuccessContainer>
+  );
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,1 +1,78 @@
-export {};
+import styled from 'styled-components';
+import { pxToRem } from '../../styles/functions/func';
+
+export const SuccessContainer = styled.div`
+  margin-top: ${pxToRem(80)};
+
+  h2 {
+    color: ${props => props.theme['yellow-dark']};
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 2rem;
+    font-weight: 800;
+    line-height: 130%;
+  }
+
+  > p {
+    color: ${props => props.theme['base-subtitle']};
+    font-size: 1.25rem;
+    line-height: 130%;
+  }
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+    margin-top: ${pxToRem(40)};
+    align-items: center;
+
+    > div {
+      padding: ${pxToRem(40)};
+      border-radius: 6px 36px;
+      background-color: ${props => props.theme.background};
+      position: relative;
+      min-width: ${pxToRem(526)};
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+
+      // BORDER GRADIENT
+      &::before {
+        content: '';
+        position: absolute;
+        inset: -1px;
+        border-radius: 7px 37px;
+        z-index: -1;
+        background: linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+      }
+
+      > div {
+        display: flex;
+        align-items: center;
+        column-gap: 0.75rem;
+
+        > div {
+          p {
+            color: ${props => props.theme['base-text']};
+            line-height: 130%;
+          }
+        }
+
+        .bold {
+          font-weight: 700;
+        }
+      }
+    }
+  }
+`;
+
+interface SuccessColors {
+  color: 'purple' | 'yellow' | 'yellow-dark';
+}
+
+export const SuccessDetails = styled.div<SuccessColors>`
+  display: flex;
+  align-items: center;
+  height: fit-content;
+  padding: 0.5rem;
+  border-radius: 100px;
+  background-color: ${props => props.theme[props.color]};
+`;
