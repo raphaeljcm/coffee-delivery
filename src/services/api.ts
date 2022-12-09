6aa3146999a3a8ddffcb247b65eefecd767515c4
Raphael Marques <raphaeljcmarques@gmail.com>
2022-12-09 17:11:00 -0300

    refactor: changing axios baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://coffee-delivery-backend.herokuapp.com/',
+  baseURL: 'https://coffee-delivery-backend-production.up.railway.app/',
   // baseURL: 'http://localhost:3333',
 });
 
